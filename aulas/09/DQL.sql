f92b8ae99d503e90c130c79ba03bba68f6ec001c
Daniel Santana <daniel_santana_87@hotmail.com>
2024-02-26 22:18:42 -0300

    Inicio DQL

---
+++ aulas/09/DQL.sql
@@ -0,0 +1,12 @@
+SELECT Nome
+FROM aluno
+WHERE matricula <10
+ORDER BY nome;
+
+SELECT * FROM ALUNO;
+
+SELECT A.Nome FROM ALUNO A;
+
+SELECT PF.Professor_ID FROM  Professores_Disciplinas PF;
+
+SELECT Nome as nm FROM Aluno;
